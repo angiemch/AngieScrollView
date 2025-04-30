070f884572124259d0bb08c79b96641fee4906f5
angiemch <angiemarilyncuchilla@gmail.com>
2025-04-30 13:59:35 -0400

    all done

--- scrollview/App.js
+++ scrollview/App.js
@@ -13,7 +13,12 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
     <View style={styles.container3}>
-      <Text>Section 2</Text>
+
+    <View style={styles.subcontainer3}/>
+    <Text style={styles.subcontainter3}>Halfs</Text>
+    <View style={styles.subcontainer3b}/>
+    <Text style={styles.subcontainer3b}>Halfs</Text>
+
       <StatusBar style="auto" />
     </View>
     <View style={styles.container4}>
@@ -25,6 +30,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  totalcontainer: { height: '100%', width: '100%' },
+
+  container: { flex: 1, flexDirection: 'row' },
+  
   container: {
     flex: 1,
     backgroundColor: 'red',
@@ -33,17 +42,26 @@ const styles = StyleSheet.create({
   },
   container2: {
     flex: 5,
-    backgroundColor: 'blue',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
     width: "100%",
   },
   container3: {
     flex: 5,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "100%"
+    backgroundColor: 'pink',
+    flexDirection: 'row'
+  },
+  subcontainer3: {
+    flex: 5,
+    backgroundColor: 'powderblue',
+    flexDirection: 'row',
+    textAlign: 'center'
+  },
+  subcontainer3b: {
+    flex: 4,
+    backgroundColor: 'pink',
+    flexDirection: 'row'
   },
   container4: {
     flex: .8,
